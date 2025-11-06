26d88592683aa23e4fa4bb0ef245ca9d0f024e0f
Felipe-Pestana <felipe.pestana@sitic.com.br>
2025-11-06 10:54:01 -0300

    Atualizado Arquivo INSERT

--- INSERT_INFOSBASICAS.sql
+++ INSERT_INFOSBASICAS.sql
@@ -29,3 +29,49 @@ INSERT INTO EspecialidadesMedicas VALUES
 ('Urologista','Cuida dos homens!');
 
 SELECT * FROM EspecialidadesMedicas
+
+INSERT INTO Medicos (CRM, Nome, Sobrenome, DataContratacao)
+VALUES
+('1234SP','João','Silva', '06/11/2025')
+INSERT INTO Medicos (CRM, Nome, Sobrenome, DataContratacao)
+VALUES
+('1234MG','Zé','Lelé', '11/13/2025')
+INSERT INTO Medicos (CRM, Nome, Sobrenome, DataContratacao)
+VALUES
+('4321SP','João','Silva', GETDATE())
+
+SELECT * FROM Medicos
+SELECT * FROM EspecialidadesMedicas
+SELECT * FROM MedicosEspecialidades
+
+INSERT INTO MedicosEspecialidades
+VALUES('10',4),(4,7)
+
+INSERT INTO TelefonesMedicos
+VALUES(55,16,12345678,4),(55,16,9876543,4)
+
+SELECT * FROM TelefonesMedicos
+
+SELECT * FROM Pacientes
+SELECT * FROM Enderecos
+
+INSERT INTO Enderecos (Logradouro, Numero, Bairro, Cidade, CEP)
+VALUES('Rua Principal',123,'Centro', 'Araraquara', 14810000)
+
+INSERT INTO Pacientes
+VALUES('Felipe','Pestana',12345,'1984-08-19',2)
+INSERT INTO Pacientes
+VALUES('Wayne','Macioski',98786,'2004-03-22',2)
+INSERT INTO Pacientes
+VALUES('Krysthian','Galbiatti',45678,'2005-12-20',2)
+
+
+SELECT * FROM TiposConsultas
+SELECT * FROM StatusConsultas
+
+
+INSERT INTO Consultas(DataHora, TipoConsulta, idStatusConsulta, 
+					Diagnostico, idPaciente, idMedico)
+VALUES(GETDATE(),1,1,'',1,4)
+
+SELECT * FROM Consultas
